5a66a9555609f45a00145cb88659bf4c281a6832
rssew <wingringo@gmail.com>
2025-11-17 14:50:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ st.header("Manager Input Section")
 st.write("Please enter the montly sales target and select the regoin")
 
 sales = st.number_input("Enter Monthly Sales Target(in USD):",
-                                                      min_value=0,
-                                                      max_value=100000,
-                                                      Value = 50000)
+                        min_value=0,
+                        max_value=100000,
+                        Value = 50000)
 
 
