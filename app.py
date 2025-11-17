47e3620f3a13c60b030c9b99717abf590d2ebed3
rssew <wingringo@gmail.com>
2025-11-17 14:48:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,12 @@ import streamlit as st
 
 st.title("Retail Business Dashboard")
 
-st.header("Header")
-st.write("Message")
+st.header("Manager Input Section")
+st.write("Please enter the montly sales target and select the regoin")
+
+sales = st.number_input("Enter Monthly Sales Target(in USD):",
+                                                      min value=0,
+                                                      max_value=100000
+                                                      Value = 50000)
+
+
