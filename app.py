85cd1bfb6ab72aa9932b0d1fbe1e148a3b746acc
rssew <wingringo@gmail.com>
2025-11-17 14:49:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.write("Please enter the montly sales target and select the regoin")
 
 sales = st.number_input("Enter Monthly Sales Target(in USD):",
                                                       min_value=0,
-                                                      max_value=100000
+                                                      max_value=100000,
                                                       Value = 50000)
 
 
